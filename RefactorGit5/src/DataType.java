58ce731613d67ce36be755d13341abc74a1127d7
dam_ndi <ivanind04@gmail.com>
2023-05-12 10:39:00 +0200

    Mod2

--- RefactorGit5/src/DataType.java
+++ RefactorGit5/src/DataType.java
@@ -11,6 +11,8 @@ public class DataType {
     }
     public static String gd(int code){
         Set<Integer> fechas = new HashSet<>(Arrays.asList(702, 1082, 1083, 1114, 1184, 1266, 12403));
+        ArrayList<Integer> numerico = new ArrayList<>(Arrays.asList(20,21,23,24,26,700,701,790,1700,2202,2203,2204,2205,2206,3734,3769,12396));
+
 
 
         if (
